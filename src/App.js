a0819550ffbce849c39a325b6bb3beab61bafe4f
pulkit <pulkit1298@gmail.com>
2020-07-24 05:31:49 +0530

    added routing to our nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
+
+const HatsPage = () => (
+  <div>
+    <h1>hats page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
